a175ca5dc6ef1c425c7d35cd65c0699ce5824664
RoyerFrba <royeryl03@gmail.com>
2024-10-09 10:39:36 -0300

    Fix images view

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
 import ProjectCard from './ProjectCard';
 import ImageGallery from './ImageGallery';
-// import ImageGallery from './ImageGallery';
+
+// Importa las imágenes directamente
+import cj1 from '/public/cj1.png';
+import cj2 from '/public/cj2.png';
+import cj3 from '/public/cj3.png';
+import cj4 from '/public/cj4.png';
+import cj5 from '/public/cj5.png';
+import cj6 from '/public/cj6.png';
+import cj7 from '/public/cj7.png';
+import cj8 from '/public/cj8.png';
+import cj9 from '/public/cj9.png';
+
+import pag2 from '/public/pag2.webp';
+import pag3 from '/public/pag3.webp';
+import pagina from '/public/pagina.png';
 
 interface Project {
   id: number;
@@ -14,37 +28,22 @@ const projectData: Project[] = [
   {
     id: 1,
     title: "Proyecto Personal - Caja Registradora",
-    description: "Este es unproyecto personal creado para comercios con el fin de tener un control de ventas y de stock , con una interfaz amigable",
-    images: [
-      "./public/cj1.png",
-      "./public/cj2.png",
-      "./public/cj3.png",
-      "./public/cj4.png",
-      "./public/cj5.png",
-      "./public/cj6.png",
-      "./public/cj7.png",
-      "./public/cj8.png",
-      "./public/cj9.png",
-    ],
+    description: "Este es un proyecto personal creado para comercios con el fin de tener un control de ventas y de stock, con una interfaz amigable",
+    images: [cj1, cj2, cj3, cj4, cj5, cj6, cj7, cj8, cj9],
   },
   {
     id: 2,
     title: "Proyecto Final e-commerce - soy henry",
-    description: "Este es un proyecto para soy henry sobre un e-commerce de sumnplementes el cual fue echo en grupo",
-    images: [
-      "./public/pag2.webp",
-      "./public/pag3.webp",
-      "./public/pagina.png",
-    ],
+    description: "Este es un proyecto para soy henry sobre un e-commerce de suplementos el cual fue hecho en grupo",
+    images: [pag2, pag3, pagina],
   },
   {
     id: 3,
     title: "Admin Control Store",
-    description: "Proycto freelancer para administrar articulos de un provedor mayorista",
-    images: [
-    ],
+    description: "Proyecto freelancer para administrar artículos de un proveedor mayorista",
+    images: [],
   },
-  // Agrega más proyectos aquí
+  // Agrega más proyectos aquí si es necesario
 ];
 
 const Projects: React.FC = () => {
@@ -54,7 +53,7 @@ const Projects: React.FC = () => {
     <section id="projects" className="projects">
       <h2>Proyectos</h2>
       <div className="project-list">
-        {projectData.map(project => (
+        {projectData.map((project) => (
           <ProjectCard
             key={project.id}
             project={project}
